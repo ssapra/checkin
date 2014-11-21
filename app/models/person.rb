1d4285415d06bc429fe4cc3d3870dd91eb121375
Shaan Sapra <ssapra@uchicago.edu>
2014-11-20 23:01:04 -0600

    Added Person model with initial rspec tests

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,15 @@
 class Person < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validate :is_a_valid_email?
+
+  private
+
+  def is_a_valid_email?
+    if !(email =~ VALID_EMAIL_REGEX)
+      errors.add(:email, "invalid email address")
+    elsif email.empty?
+      errors.add(:email, "blank email address")
+    end
+  end
 end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Person do
+
+  describe '#valid' do
+    context 'when email is valid' do
+      before do
+        @person = Person.create(email: 'ssapra@uchicago.edu')
+      end
+
+      it 'is valid' do
+        expect(@person).to be_valid
+      end
+    end
+  end
+
+  describe '#invalid' do
+    context 'when email is empty' do
+      before do
+        @person = Person.create(email: '')
+      end
+
+      it 'is invalid' do
+        expect(@person).to_not be_valid
+      end
+
+      it 'should have an error on the email attribute' do
+        expect(@person.errors[:email].size).to eq 1
+      end
+    end
+
+    context 'when the email is not formatted correctly' do
+      before do
+        @person = Person.create(email: 'nonense@google')
+      end
+
+      it 'is invalid' do
+        expect(@person).to_not be_valid
+      end
+
+      it 'should have an error on the email attribute' do
+        expect(@person.errors[:email].size).to eq 1
+      end
+    end
+  end
+
+end
